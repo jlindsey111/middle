afb5aad6bd3289ffee6b75e2f172efb88b9fcbd6
Jadeyn Lindsey <jadeynlindsey@gmail.com>
2024-09-27 20:09:37 +0000

    added how to use script

--- middle.sh
+++ middle.sh
@@ -1,6 +1,7 @@
 # script to return middle txt of a file
 # created 9/27/2024
 # creator Jadeyn Lindsey
-
+# Usage: 
+# include in first position, file path and name via command line 
 
 head -n 9 "$1" | tail -n 3
